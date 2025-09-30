f0e565fc49e070378835f9bca3e9d59e51c94009
NgGuanZhe <nggz321@gmail.com>
2025-09-29 21:22:51 -0700

    fix automated prep.sh file

--- prox_frontend/vite.config.js
+++ prox_frontend/vite.config.js
@@ -1,26 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dotenv from 'dotenv'
+dotenv.config({path: '../.env'})
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  // This line loads the .env file
+  process.env = {...process.env, ...loadEnv(mode, process.cwd() + '/prox_frontend', '.env')};
+  return defineConfig({
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          // This line uses the variable
+          target: process.env.VITE_API_TARGET,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      }
     }
-  },
-  server: {
-    proxy: {
-      // Proxy all requests starting with /api to your backend server
-      '/api': {
-        target: 'http://192.168.170.130:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
-  }
-})
+  })
+}
